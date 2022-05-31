e1c084545e94c8690d6e11caab128312885274c4
陈群矜 <1691876150@qq.com>
2022-05-31 09:09:59 +0800

    配置https

--- gateway/src/main/java/com/meeting/gateway/config/Config.java
+++ gateway/src/main/java/com/meeting/gateway/config/Config.java
@@ -1,7 +1,13 @@
 package com.meeting.gateway.config;
 
 import com.meeting.common.util.JwtTokenUtil;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -21,6 +27,40 @@ public class Config {
         builder.setConnectTimeout(Duration.ofMillis(8 * 1000));
         builder.setReadTimeout(Duration.ofMillis(8 * 1000));
         return builder.build();
+    }
+
+    /**
+     * http重定向到https
+     * @return
+     */
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                collection.addMethod(DEFAULT_PROTOCOL);
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(8080);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(8443);
+        return connector;
     }
 
 }
